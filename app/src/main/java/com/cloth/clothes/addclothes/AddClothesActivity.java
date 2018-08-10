13372dc29c2b520fb32e7528a3e361b28819f93a
xumingqiang <xumingqiang@okay.cn>
2018-08-10 18:53:37 +0800

    [add] 增加add

---
+++ app/src/main/java/com/cloth/clothes/addclothes/AddClothesActivity.java
@@ -0,0 +1,15 @@
+package com.cloth.clothes.addclothes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cloth.clothes.R;
+
+public class AddClothesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_clothes);
+    }
+}
